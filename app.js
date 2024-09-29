f1395888b944caa2b0be37881edd5326dd405fd8
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2024-09-29 23:09:51 +0200

    removed 3000 + json

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use((err, req, res, next) => {
   });
   
 
-//Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+// //Start Server
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
